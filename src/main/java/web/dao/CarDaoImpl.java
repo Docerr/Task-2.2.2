184993bfe840bbe61e21f4e05f851aa2f972993c
Docerr <doc11233@gmail.com>
2022-10-01 12:35:28 +0300

    init commit5

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -15,12 +15,12 @@ public class CarDaoImpl implements CarDao {
         new Car("Lada", 100000000, 20),
         new Car("Tesla", 2, 1),
         new Car("Mercedes", 30, 23),
-        new Car("RenaultRamilya", 2000000001, 11),
-        new Car("RenaultRamilya2", 2000000002, 11),
-        new Car("RenaultRamilya3", 2000000003, 11),
-        new Car("RenaultRamilya4", 2000000004, 11),
-        new Car("RenaultRamilya5", 2000000005, 11),
-        new Car("RenaultRamilya6", 2000000006, 11)
+        new Car("RenaultRamilya", 2000000001, 11)
+//        new Car("RenaultRamilya2", 2000000002, 11),
+//        new Car("RenaultRamilya3", 2000000003, 11),
+//        new Car("RenaultRamilya4", 2000000004, 11),
+//        new Car("RenaultRamilya5", 2000000005, 11),
+//        new Car("RenaultRamilya6", 2000000006, 11)
 
     );
 
